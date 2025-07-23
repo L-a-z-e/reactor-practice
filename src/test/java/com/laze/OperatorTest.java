6f019b3bd94fe7ddad7cb44f7295a6d033e05acb
L-a-z-e <yysi8771@gmail.com>
2025-07-23 23:41:49 +0900

    operator

---
+++ src/test/java/com/laze/OperatorTest.java
@@ -0,0 +1,46 @@
+package com.laze;
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperatorTest {
+
+    private final Operator operator = new Operator();
+
+    @Test
+    void fluxMap() {
+        StepVerifier.create(operator.fluxMap())
+                .expectNext(2,4,6,8,10,12,14,16,18,20)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxFilter() {
+        StepVerifier.create(operator.fluxFilter())
+                .expectNext(6,7,8,9,10)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxFilterTake() {
+        StepVerifier.create(operator.fluxFilterTake())
+                .expectNext(6, 7, 8)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxFlatMap() {
+        StepVerifier.create(operator.fluxFlatMap())
+                .expectNextCount(100)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxFlatMap2() {
+        StepVerifier.create(operator.fluxFlatMap2())
+                .expectNextCount(81)
+                .verifyComplete();
+    }
+}
